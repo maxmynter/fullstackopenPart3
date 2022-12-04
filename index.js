94ba543c3c94b4b79b81908cae8437870ffd285d
Max Mynter <maxmynter@me.com>
2022-12-04 22:07:30 +0100

    Ex 3.11

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ morgan.token("req-body", (req, res) => {
   return JSON.stringify(req.body);
 });
 app.use(cors());
+app.use(express.static("build"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :req-body"
@@ -82,7 +83,7 @@ app.post("/api/persons/", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
