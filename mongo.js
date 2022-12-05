977de3e1ab3c4cdf8990f0fb5e334bafdbdb7e2a
Max Mynter <maxmynter@me.com>
2022-12-05 16:09:00 +0100

    Exercise 3.12, Add mongodb

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://maxmynter:${password}@cluster0.d8ctx4a.mongodb.net/?retryWrites=true&w=majority`;
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length == 5) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log("connected");
+
+      const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+      });
+
+      return person.save();
+    })
+    .then((person) => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`);
+      return mongoose.connection.close();
+    })
+    .catch((err) => console.log(err));
+}
+
+if (process.argv.length == 3) {
+  mongoose
+    .connect(url)
+    .then((connection) => {
+      console.log("Phonebook:");
+      Person.find({}).then((person) => {
+        person.forEach((result) => console.log(result));
+        return mongoose.connection.close();
+      });
+    })
+    .catch((err) => console.log(err));
+}
